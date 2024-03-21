f036e61044dd184098ddd7747e68f3d9aa8d7374
Sahil <sahilkkuthe2004@gmail.com>
2024-03-21 23:36:08 +0530

    Signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,31 +19,42 @@ app.post('/api/v1/user/signup', async (c) => {
   }).$extends(withAccelerate())
 
   const body = await c.req.json();
-
-
   const user = await prisma.user.create({
     data: {
       email: body.email,
       password: body.password
     }
   });
-
   const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-  
-
   return c.json({jwt: token}); 
-    
   
-
-
-  
-
-  return c.text("signup")
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text("signin")
-})
+
+
+app.post('/api/v1/user/signin', async (c) => {        // email and pass validation remaining....
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {email: body.email}
+  })
+
+  if (!user) {
+    c.status(403)
+    return c.json({error: "User not found"})
+  }
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+  return c.json({token})
+
+}) 
+
+
+
+
 
 app.post('api/v1/blog', (c) => {
   return c.text("blog")
